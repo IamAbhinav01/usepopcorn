646018c46e3b9a1193bdc0ff5fa4a2b9c833271a
Abhinav <AbhinavSunil@hotmail.com>
2025-11-19 11:12:42 +0530

    created custom component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 import StarRating from './StarRating';
-
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="red" maxRating={20} onsSetRating={setMovieRating} />
+      <p>this movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );
