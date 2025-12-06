e3e482def28a0690dc4d0f4ec0738412ac530cc8
Abhinav <AbhinavSunil@hotmail.com>
2025-12-06 11:33:33 +0530

    add abort

--- src/App.js
+++ src/App.js
@@ -227,6 +227,9 @@ function SelectedMovieList({
     function () {
       if (!title) return;
       document.title = `Movie | ${title}`;
+      return function () {
+        document.title = 'usePopcorn';
+      };
     },
     [title]
   );
@@ -379,13 +382,15 @@ export default function App() {
   }
   useEffect(
     function () {
+      const controller = new AbortController();
       async function fetchMovies() {
         try {
           setLoading(true);
           setError('');
 
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok) throw new Error('Something went wrong fetching movies');
@@ -396,7 +401,9 @@ export default function App() {
 
           setMovies(data.Search);
         } catch (err) {
-          setError(err.message);
+          if (err.name !== 'AbortError') {
+            setError(err.message);
+          }
         } finally {
           setLoading(false);
         }
@@ -409,6 +416,9 @@ export default function App() {
       }
 
       fetchMovies();
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
